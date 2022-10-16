4dbb258fff24c36d00d5f54df1e2bc9ac873a7ef
Javier Herrero <javier.ig.herrero@gmail.com>
2022-10-16 09:50:29 -0300

    se trabaja en index

--- style.js
+++ style.js
@@ -1,12 +1,30 @@
-var textos = [
-    "Mis gafas en el escritorio",
-    "Camino a la perdición",
-    "Paseando en bicicleta",
-    "Vias de el tren"
-]
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-function cargarfoto(img, txt) {
-    document.getElementById("galeria").src = "https://picsum.photos/id/" + img + "/400/400";
-    document.getElementById("mitexto").innerHTML = textos[txt];
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
